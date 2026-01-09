390f49a631f6008d84f794ddbfbf03cb80241b09
hilal_dinc <hilaldincc94@gmail.com>
2026-01-09 13:22:14 +0300

    feat(frontend): create RegistrationPage to render RegistrationForm component

---
+++ apps/frontend/src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,7 @@
+import RegistrationForm from "../../components/Auth/RegistrationForm/RegistrationForm.jsx";
+
+const RegistrationPage = () => {
+  return <RegistrationForm />;
+};
+
+export default RegistrationPage;
