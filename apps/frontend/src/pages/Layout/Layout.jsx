b6e319e2661a189d6326795aa63a7cf15d87baf5
cucuhead <burcubudak13@gmail.com>
2026-01-09 21:24:36 +0300

    Render Modal globally in Layout

--- apps/frontend/src/pages/Layout/Layout.jsx
+++ apps/frontend/src/pages/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header.jsx";
-
+import Modal from "../../components/Modal/Modal";
 const Layout = () => {
   return (
     <div>
@@ -9,6 +9,8 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
+            <Modal />
+
     </div>
   );
 };
